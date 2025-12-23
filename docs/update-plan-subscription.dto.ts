0bb7db032f01e84fe3aaa05008fe4c753481412a
axel valles <agabrielv1799@gmail.com>
2025-12-22 20:42:57 -0500

    Remove plan and subscription documentation files
    Deleted DTOs, entities, controllers, and module files related to plans and plan subscriptions from the docs directory. This cleanup likely reflects a restructuring or migration of plan-related documentation and code.

--- docs/update-plan-subscription.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePlanSubscriptionDto } from './create-plan-subscription.dto';
-
-export class UpdatePlanSubscriptionDto extends PartialType(
-  CreatePlanSubscriptionDto,
-) {}
